0bb699ab99da14f9b1e27af4aa9b06d84f9eb93d
Swarup9437 <71583930+Swarup9437@users.noreply.github.com>
2022-05-24 23:59:25 +0530

    Update bitManupulation.java

--- bitManupulation.java
+++ bitManupulation.java
@@ -2,5 +2,5 @@ BitMask = 1 << position;
 GET = num & BitMask;
 SET = num | BitMask;
 CLEAR = num & ~(BitMask);
-UPDATE(0) = num | BitMask;
-UPDATE(1) = num & ~(BitMask);
+UPDATE(1) = num | BitMask;
+UPDATE(0) = num & ~(BitMask);
